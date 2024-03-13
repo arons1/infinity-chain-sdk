87481b0c3c80349e935909da01d540459dd29433
Autojadame <autojadame@gmail.com>
2024-03-13 14:57:40 +0100

    vechain tokens

--- src/transactionParsers/vechain/general/pull.ts
+++ src/transactionParsers/vechain/general/pull.ts
@@ -1,27 +1,13 @@
-import { PROVIDER } from '../constants';
 import { GeneralApiParams } from '../../types';
 
 export const pull = ({
-    chainId,
-    apiKey,
     address,
     page,
     limit,
 }: GeneralApiParams) => {
-    const selected = PROVIDER[chainId as number] as string;
-    if (!selected) throw new Error('Not integrated chain');
     return {
         method: 'GET',
-        url:
-            PROVIDER[chainId as number] +
-            '/api?module=account&apikey=' +
-            apiKey +
-            '&action=txlist&address=' +
-            address +
-            '&startblock=1&endblock=99999999999999999999999&sort=desc&page=' +
-            page +
-            '&offset=' +
-            limit,
+        url:`https://explore.vechain.org/api/accounts/${address.toLowerCase()}/transactions?limit=${limit}${"&offset="+(page ?? 0)}`
     };
 };
 
---
+++ src/transactionParsers/vechain/tokens/pull.ts
@@ -0,0 +1,14 @@
+import { GeneralApiParams } from '../../types';
+
+export const pull = ({
+    address,
+    page,
+    limit,
+}: GeneralApiParams) => {
+    return {
+        method: 'GET',
+        url:`https://explore.vechain.org/api/accounts/${address.toLowerCase()}/transfers?limit=${limit}${"&offset="+(page ?? 0)}`
+    };
+};
+
+
