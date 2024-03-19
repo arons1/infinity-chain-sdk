e65269044ea36e661c72e629da925cbfbc368376
Autojadame <autojadame@gmail.com>
2024-03-19 13:54:57 +0100

    cc

--- src/networks/xrp/getBalance/index.ts
+++ src/networks/xrp/getBalance/index.ts
@@ -1,9 +1,11 @@
+import BigNumber from "bignumber.js";
+import { CurrencyBalanceResult } from "../../types";
 import { GetBalanceParams } from "./types";
 
 export const getBalance = async ({
     api,
     address
-}:GetBalanceParams) => {
+}:GetBalanceParams) : Promise<CurrencyBalanceResult> => {
     const request = {
         command: 'account_info',
         account: address,
@@ -14,14 +16,18 @@ export const getBalance = async ({
           timeoutSeconds:5
         })
         const result = data.error == "actNotFound" ? 0 : data.error ? 0 : data.account_data["Balance"];
+        const base = api.getState().reserve.base as number
+        const owner = api.getState().reserve.owner as number
+        const reserve = new BigNumber(base + owner).shiftedBy(6).toNumber()
         return {
-            balance:result
+            balance:result,
+            available:new BigNumber(result).minus(reserve).toString(10)
         }
       }
       catch(e){
         console.error(e)
         return {
-            balance:0
+            balance:"0"
         }
       }
 }
