6a46f1be17424c7a32e64b35b1fa6277eb0d4feb
Autojadame <autojadame@gmail.com>
2024-03-20 17:12:08 +0100

    tests

---
+++ test/networks/fio/index.test.ts
@@ -0,0 +1,10 @@
+import { describe, expect, test } from '@jest/globals';
+
+const mnemonic =
+    'derive lab over dragon nothing pioneer until deputy inherit help next release';
+
+describe('networksEVM', () => {
+    test('generateStellarAddress', async () => {
+        expect(true).toBe(true)
+    })
+})
---
+++ test/transactionParsers/kcc/index.test.ts
@@ -0,0 +1,7 @@
+import { describe, expect, test } from '@jest/globals';
+
+describe('networksEVM', () => {
+    test('generateStellarAddress', async () => {
+        expect(true).toBe(true)
+    })
+})
