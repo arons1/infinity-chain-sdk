9206a841c8ce779599d13d8f7d50094435ca5bb7
Autojadame <autojadame@gmail.com>
2024-04-21 17:53:34 +0200

    removed transaction tests here, as it will be part of the service of historical

--- test/transactionParsers/fio/index.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const globals_1 = require("@jest/globals");
-(0, globals_1.describe)('networksEVM', () => {
-    (0, globals_1.test)('generateStellarAddress', async () => {
-        (0, globals_1.expect)(true).toBe(true);
-    });
-});
--- test/transactionParsers/kcc/index.test.ts
+++
@@ -1,7 +0,0 @@
-import { describe, expect, test } from '@jest/globals';
-
-describe('networksEVM', () => {
-    test('generateStellarAddress', async () => {
-        expect(true).toBe(true);
-    });
-});
