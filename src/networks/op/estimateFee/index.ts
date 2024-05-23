05e1122de1cce11b90cd4396fd75686512bdfb09
Autojadame <autojadame@gmail.com>
2024-05-23 16:21:03 +0200

    estimate handle

--- src/networks/op/estimateFee/index.ts
+++ src/networks/op/estimateFee/index.ts
@@ -2,6 +2,7 @@ import feeAbi from '@infinity/core-sdk/lib/commonjs/core/abi/fee';
 import Web3 from 'web3';
 import { BigNumber } from '@infinity/core-sdk/lib/commonjs/core';
 import { estimateFeeParametersChecker } from '../parametersChecker';
+import { CannotEstimateTransaction } from '../../../errors/networks';
 
 /**
  * Estimates L1 Cost
@@ -18,7 +19,14 @@ export const estimateL1Cost = async (
         feeAbi,
         '0x420000000000000000000000000000000000000F',
     );
-    return new BigNumber(
-        await gpo.methods.getL1Fee(Buffer.from(rawTransaction, 'hex')).call(),
-    ).toString(10);
+    try{
+        return new BigNumber(
+            await gpo.methods.getL1Fee(Buffer.from(rawTransaction, 'hex')).call(),
+        ).toString(10);
+    }
+    catch(e){
+        console.error(e)
+        throw new Error(CannotEstimateTransaction)
+    }
+
 };
